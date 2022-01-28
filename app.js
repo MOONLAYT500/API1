9cbe58f26f8e0a99d38a99975f313f9baad8c485
MOONLAYT500 <mazajanton1@gmail.com>
2022-01-28 19:36:25 +0300

    crud realised

--- app.js
+++ app.js
@@ -1,20 +1,71 @@
 const express = require('express');
+const req = require('express/lib/request');
 const app = express();
-
+const fs = require('fs');
+const { v4 } = require('uuid');
 const port = 3000;
+const jsonParser = express.json();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.use(express.static(__dirname + '/public'));
+
+const filePath = 'todos.json';
+
+// get all todos
+app.get('/api/todos', (req, res) => {
+  const content = fs.readFileSync(filePath, 'utf8');
+  const todos = JSON.parse(content);
+  res.send(todos);
 });
 
-app.get('/about', (req, res) => {
-  res.send('about');
+
+// add new todo
+app.post('/api/todos', jsonParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+
+  const todoName = req.body.name;
+  const todoDone = req.body.done;
+  let todo = { 
+    uuid:v4(), 
+    name: todoName, 
+    done: todoDone, 
+    createdAt:+new Date()
+  };
+
+  let data = fs.readFileSync(filePath, 'utf8');
+  let todos = JSON.parse(data);
+
+  todos.push(todo);
+  data = JSON.stringify(todos);
+  fs.writeFileSync('todos.json', data);
+  res.send(todo);
 });
 
-app.get('/contacts', (req, res) => {
-  res.send('contacts');
+// delete
+app.delete('/api/todos/:id', (req, res) => {
+  const id = req.params.id;
+  let data = fs.readFileSync(filePath, 'utf8');
+  let todos = JSON.parse(data);
+  todos = todos.filter((todo) => todo.uuid !== id);
+  data = JSON.stringify(todos);
+  fs.writeFileSync('todos.json', data);
+});
+// patch
+
+
+app.patch('/api/todos/:id', jsonParser, (req, res) => {
+  const id = req.params.id;
+  let data = fs.readFileSync(filePath, 'utf8');
+  let todos = JSON.parse(data);
+  let todo = todos.find((todo) => todo.uuid == id);
+  todo.name = req.body.name
+  todo.done = req.body.done
+  index = todos.findIndex((todo) => todo.uuid == id);
+  todos.splice(index, 1, todo);
+  data = JSON.stringify(todos);
+  fs.writeFileSync('todos.json', data);
+  res.send(todo);
 });
 
 app.listen(port, () => {
-  console.log('fesfsfse');
+  console.log(`${port}`);
 });

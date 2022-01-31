878026066d1d57797b0fff8f5f5fd25670b07f4b
MOONLAYT500 <mazajanton1@gmail.com>
2022-01-31 17:57:42 +0300

    new router

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const getTodos = require('./routes/read')
 const createTodo = require('./routes/create')
 const updateTodo = require('./routes/update');
 const deleteTodo = require('./routes/delete');
-
+const router = require('./router')
 
 const port = 3000;
-
-app.use('/api', getTodos)
-app.use('/api', createTodo)
-app.use('/api',deleteTodo)
-app.use('/api',updateTodo)
+app.use('/api', router)
+// app.use('/api', getTodos)
+// app.use('/api', createTodo)
+// app.use('/api',deleteTodo)
+// app.use('/api',updateTodo)
 
 app.listen(port, () => {
   console.log(`${port}`);

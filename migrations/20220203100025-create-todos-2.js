b82a72c873672172bd711e2b72ce37017d404781
MOONLAYT500 <mazajanton1@gmail.com>
2022-02-03 13:59:05 +0300

    new migration

--- migrations/20220203100025-create-todos-2.js
+++ migrations/20220203100025-create-todos-2.js
@@ -2,7 +2,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable(
-      'todos2s',
+      'todos',
       {
         uuid: {
           type: Sequelize.UUID,
@@ -30,6 +30,6 @@ module.exports = {
     );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('todos2s');
+    await queryInterface.dropTable('todos');
   },
 };

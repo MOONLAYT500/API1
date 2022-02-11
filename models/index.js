7b21e849b5e7c7f521d1c195593d8181ea22d08b
MOONLAYT500 <mazajanton1@gmail.com>
2022-02-11 14:29:42 +0300

    new index model

--- models/index.js
+++ models/index.js
@@ -6,31 +6,31 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
 const config = require('../config/config');
-// const {
-//     dialect,
-//     database,
-//     username,
-//     password,
-//     host,
-// } = require('../config/config');
+const {
+    dialect,
+    database,
+    username,
+    password,
+    host,
+} = require('../config/config');
 
-// const sequelize = new Sequelize(database, username, password, {
-//     dialect: dialect,
-//     host: host,
-//     ssl: true,
-// });
+const sequelize = new Sequelize(database, username, password, {
+    dialect: dialect,
+    host: host,
+    ssl: true,
+});
 
-let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(
-        config.database,
-        config.username,
-        config.password,
-        config
-    );
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//     sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//     sequelize = new Sequelize(
+//         config.database,
+//         config.username,
+//         config.password,
+//         config
+//     );
+// }
 
 fs.readdirSync(__dirname)
     .filter((file) => {
